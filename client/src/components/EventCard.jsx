adfdaa3bfe5e430e402d54e5aaf2aefed566e418
abhaysingh8904 <abhaybhadoriya90@gmail.com>
2026-01-11 23:55:22 +0530

    Added React frontend

--- client/src/components/EventCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function EventCard({ event }) {
-  return (
-    <div>
-      <h2>{event.title}</h2>
-      <p>{event.location}</p>
-      <p>Seats: {event.available_seats}</p>
-    </div>
-  );
-}
